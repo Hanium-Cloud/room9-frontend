b48625ada372c83c0f0d686fe669bc7ca8b9b7e5
zooneon <snsxhdz@naver.com>
2021-09-27 01:39:23 +0900

    뷰 수정 및 검색 기능 구현

--- src/views/main/guest/SearchResult.js
+++ src/views/main/guest/SearchResult.js
@@ -1,31 +1,47 @@
-import {Col, Input, Row, Slider, InputNumber, Button} from "antd";
-import {SearchOutlined} from "@ant-design/icons";
+import {Col, Input, Row, Slider, InputNumber, Button, Menu, Dropdown} from "antd";
+import {SearchOutlined, DownOutlined} from "@ant-design/icons";
 import Color from "../../../constant/Color";
 import React, {useState, useEffect} from "react";
 import PageHeader from "../../components/PageHeader";
-import AppHeader from "../../layouts/AppHeader";
 import BottomNavigation from "../../layouts/BottomNavigation";
 import RoomCard from "../../main/room/RoomCard"
 import MockData from "../../../constant/MockData";
 import styled from 'styled-components';
 import {getSearchResult} from "../../../api/search";
-import {getAllRooms} from "../../../api/room";
+import TopNavigation from "../../layouts/TopNavigation";
+import { useRecoilState } from "recoil";
+import { filterState } from "../../../store/state";
 
 const SearchBoxBlock = styled.div `
-    padding: '10px 20px';
+    margin-bottom: 10px;
     overflow: 'hidden';
     display: grid;
     grid-template:
-        "InputBlock InputBlock" 50%
-        "Slider InputNumber" 50%
-        "Button Button"
-        /1fr 1fr;
+        "InnerInputBox" 50%
+        "InnerBox" 50%
+        "Button"
+        /1fr;
+`;
+
+const InnerInputBox = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const InnerOtherBox = styled.div `
+    display: grid;
+    grid-template:
+        "SliderBox InputBox DropdownBox"
+        /1fr 1fr 0.75fr;
 `;
 
 const InputBox = styled.div `
     display: flex;
     justify-content: center;
     align-items: center;
+    padding: 0px 15px 0px 15px;
+    width: 100%;
 `;
 
 const SliderBox = styled.div `
@@ -34,17 +50,18 @@ const SliderBox = styled.div `
         "p" 25%
         "Slider" 75%
         /1fr;
+    width: 100%;
+`;
+
+const DropDownBox = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const SearchResult = (props) => {
+    const [filter, setFilter] = useRecoilState(filterState);
     const [searchRooms, setSearchRooms] = useState([]);
-    const [filter, setFilter] = useState({
-        title: '',
-        limitPrice: 0,
-        detailLocation: '',
-        limitPeople: 0,
-        orderStandard: null
-    });
     const [visible, setVisible] = useState(true);
 
     const onInputClick = e => {
@@ -83,64 +100,99 @@ const SearchResult = (props) => {
       
             const rooms = response.data.room;
             const results = rooms.map(room => ({
-              'roomId' : room.roomId,
-              'imageUrl' : room?.images[0]?.url,
-              'description' : room.title
-            }));
+                'id': room.roomId,
+                'thumbnailUrl': room?.images[0]?.url,
+                'name': room.title,
+                'region': room.location,
+                'price': room.price,
+                'score': 3.58,
+                'reviewCount': room.like,
+              }));
       
             setSearchRooms(results);
           });
+    };
+
+    const onDropdownClick = e => {
+        const sorting = e.target.textContent === '최신순' ? 'CREATEDDESC' : 'LIKEDDESC';
         setFilter({...filter,
-            title: '',
-            limitPrice: 0,
-            detailLocation: '',
-            limitPeople: 0,
-            orderStandard: null
+            orderStandard: sorting
         });
     };
 
     useEffect(() => {
-        getAllRooms().then((response) => {
+        getSearchResult(filter).then((response) => {
             setSearchRooms([]);
       
             const rooms = response.data.room;
             const results = rooms.map(room => ({
-              'roomId' : room.roomId,
-              'imageUrl' : room?.images[0]?.url,
-              'description' : room.title
-            }));
+                'id': room.roomId,
+                'thumbnailUrl': room?.images[0]?.url,
+                'name': room.title,
+                'region': room.location,
+                'price': room.price,
+                'score': 3.58,
+                'reviewCount': room.like,
+              }));
       
             setSearchRooms(results);
           });
     }, []);
 
+    const menu = (
+        <Menu>
+          <Menu.Item>
+            <div onClick={onDropdownClick}>
+              최신순
+            </div>
+          </Menu.Item>
+          <Menu.Item>
+            <div onClick={onDropdownClick}>
+              좋아요순
+            </div>
+          </Menu.Item>
+        </Menu>
+    );
+
     return (
       <div style={{paddingBottom: '70px'}}>
-        <AppHeader />
+        <TopNavigation title="방 검색" />
         <Row>
-            <Col span={24} style={{padding: '15px', backgroundColor: Color.Primary} }>
-                <Input size="large" style={{borderRadius: '25px'}} prefix={<SearchOutlined style={{color: '#888888'}}/>} placeholder="마음에 드는 숙소를 찾아보세요!" onClick={onInputClick} readOnly/>
+            <Col span={24} style={{padding: '0px 15px 10px 15px'} }>
+                <Input size="medium" style={{borderRadius: '25px', borderColor: Color.Primary}} prefix={<SearchOutlined style={{color: '#888888'}}/>} placeholder="필터로 검색하기" onClick={onInputClick} readOnly/>
             </Col>
         </Row>
         {visible ? (
             <SearchBoxBlock>
-                <InputBox>
-                    <Input style={{width: '75%', borderColor: Color.Primary, borderRadius: '25px'}} placeholder="방 이름으로 검색" onChange={onTitleInputChange}/>
-                </InputBox>
-                <InputBox>
-                    <Input style={{width: '75%', borderColor: Color.Primary, borderRadius: '25px'}} placeholder="지역 이름으로 검색" onChange={onLocationInputChange}/>
-                </InputBox>
-                <SliderBox>
-                    <div style={{marginLeft: '35%', color: "gray"}}>가격 설정</div>
-                    <Slider style={{width: '75%', borderColor: Color.Primary, marginLeft: '10%'}} min={1} max={30} onChange={onSliderChange}/>
-                </SliderBox>
-                <InputBox>
-                    <InputNumber style={{width: "75%", borderColor: Color.Primary, borderRadius: '25px'}} placeholder="예약 인원 수 선택" min={1} max={10} onChange={onInputNumberChange}/>
-                </InputBox>
-                <Button size="medium" icon="검색" style={{width: "200%", border: "none", backgroundColor: Color.Primary, color: "white"}} onClick={onButtonClick}/>
+                <InnerInputBox>
+                    <InputBox>
+                        <Input style={{width: '120%', borderColor: Color.Primary, borderRadius: '25px'}} placeholder="방 이름으로 검색" onChange={onTitleInputChange} value={filter.title}/>
+                    </InputBox>
+                    <InputBox>
+                        <Input style={{width: '160%', borderColor: Color.Primary, borderRadius: '25px'}} placeholder="지역 이름으로 검색" onChange={onLocationInputChange} value={filter.detailLocation}/>
+                    </InputBox>
+                </InnerInputBox>
+                <InnerOtherBox>
+                    <SliderBox>
+                        <div style={{marginLeft: '35%', color: "gray"}}>가격 설정</div>
+                        <Slider style={{width: '75%', borderColor: Color.Primary, marginLeft: '10%'}} min={10000} max={100000} step={5000} onChange={onSliderChange} value={filter.limitPrice}/>
+                    </SliderBox>
+                    <InputBox>
+                        <InputNumber style={{width: "100%", borderColor: Color.Primary, borderRadius: '25px'}} placeholder="예약 인원 수 선택" min={1} max={10} onChange={onInputNumberChange} value={filter.limitPeople}/>
+                    </InputBox>
+                    <DropDownBox>
+                        <Dropdown overlay={menu} style={{width: '20%'}}>
+                            <a className="ant-dropdown-link" onClick={e => e.preventDefault()} style={{color: Color.Primary}}>
+                                {filter.orderStandard === 'CREATEDDESC' ? '최신순' : '좋아요순'} <DownOutlined />
+                            </a>
+                        </Dropdown>
+                    </DropDownBox>
+                </InnerOtherBox>
+                <Button size="medium" icon="검색" style={{width: "95%", borderColor: Color.Primary, color: Color.Primary, margin: '10px 0px 0px 15px', borderRadius: '25px'}} onClick={onButtonClick}/>
             </SearchBoxBlock>
         ) : null}
-        {searchRooms === null ? (
+        {searchRooms === null ? 
+            <PageHeader title="검색 결과가 없습니다."/> : (
             <>
                 <PageHeader title="검색 결과"/>
                 <div>
@@ -151,7 +203,7 @@ const SearchResult = (props) => {
                 }
                 </div>
             </>   
-        ) : <PageHeader title="검색 결과가 없습니다."/>}
+        )}
         <BottomNavigation />
       </div>
     );
